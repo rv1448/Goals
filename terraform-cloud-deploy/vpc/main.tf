629fd19e4338b93c5dacf75354eda30f8893dafb
rahul <r.reddy1448@gmail.com>
2020-10-04 09:13:22 -0400

    new VPC

--- terraform-cloud-deploy/vpc/main.tf
+++ terraform-cloud-deploy/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "vpc_design"{
     cidr_block = var.prod_vpc
     enable_dns_hostnames = true
 
-    tags {
+    tags = {
         Name="Productionvpc"
     }
 }
@@ -15,8 +15,8 @@ resource "aws_vpc" "vpc_design"{
 resource "aws_subnet" "pbsn_1"{
     cidr_block = var.public_subnet_1
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}a"
-    tags {
+    availability_zone = "${var.aws_region}a"
+    tags = {
         Name="Public-subnet-1"
     }
 }
@@ -24,8 +24,8 @@ resource "aws_subnet" "pbsn_1"{
 resource "aws_subnet" "pbsn_2"{
     cidr_block = var.public_subnet_2
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}b"
-    tags {
+    availability_zone = "${var.aws_region}b"
+    tags = {
         Name="Public-subnet-2"
     }
 }
@@ -33,8 +33,8 @@ resource "aws_subnet" "pbsn_2"{
 resource "aws_subnet" "pbsn_3"{
     cidr_block = var.public_subnet_3
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}c"
-    tags {
+    availability_zone = "${var.aws_region}c"
+    tags = {
         Name="Public-subnet-3"
     }
 }
@@ -43,8 +43,8 @@ resource "aws_subnet" "pbsn_3"{
 resource "aws_subnet" "pvsn_1"{
     cidr_block = var.private_subnet_1
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}a"
-    tags {
+    availability_zone = "${var.aws_region}a"
+    tags = {
         Name="Private-subnet-1"
     }
 }
@@ -52,17 +52,66 @@ resource "aws_subnet" "pvsn_1"{
 resource "aws_subnet" "pvsn_2"{
     cidr_block = var.private_subnet_2
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}b"
-    tags {
+    availability_zone = "${var.aws_region}b"
+    tags = {
         Name="Private-subnet-2"
     }
 }
 
-esource "aws_subnet" "pvsn_3"{
+resource "aws_subnet" "pvsn_3"{
     cidr_block = var.private_subnet_3
     vpc_id = aws_vpc.vpc_design.id
-    availability_zone = "{var.aws_region}c"
-    tags {
+    availability_zone = "${var.aws_region}c"
+    tags = {
         Name="Private-subnet-3"
     }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.vpc_design.id
+    tags = {
+        Names="public-route-table"
+    }
+}
+
+resource "aws_route_table" "private_route_table" {
+    vpc_id = aws_vpc.vpc_design.id
+    tags = {
+        Names="private-route-table"
+    }
+}
+
+resource "aws_route_table_association" "public_subnet_1_association"{
+    route_table_id = aws_route_table.public_route_table.id
+    subnet_id = aws_subnet.pbsn_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association"{
+    route_table_id = aws_route_table.public_route_table.id
+    subnet_id = aws_subnet.pbsn_2.id
+}
+
+resource "aws_route_table_association" "public_subnet_3_association"{
+    route_table_id = aws_route_table.public_route_table.id
+    subnet_id = aws_subnet.pbsn_3.id
+}
+
+
+
+
+
+resource "aws_route_table_association" "private_subnet_1_association"{
+    route_table_id = aws_route_table.private_route_table.id
+    subnet_id = aws_subnet.pvsn_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association"{
+    route_table_id = aws_route_table.private_route_table.id
+    subnet_id = aws_subnet.pvsn_2.id
+}
+
+resource "aws_route_table_association" "private_subnet_3_association"{
+    route_table_id = aws_route_table.private_route_table.id
+    subnet_id = aws_subnet.pvsn_3.id
 }
--- terraform-cloud-deploy/vpc/variable.tf
+++ terraform-cloud-deploy/vpc/variable.tf
@@ -2,22 +2,41 @@ variable "aws_region" {
     default = "us-east-2"
 }
 
+variable "prod_vpc" {
+    default = "10.0.0.0/16"
+}
 
-variable "public-subnet-1" {
-    default =
+variable "public_subnet_1" {
+    default = "10.0.0.0/19"
 }
-variable "public-subnet-2" {
-    default =
+variable "public_subnet_2" {
+    default = "10.0.32.0/19"
 }
-variable "public-subnet-3" {
-    default =
+variable "public_subnet_3" {
+    default = "10.0.64.0/19"
 }
-variable "private-subnet-1" {
-    default =
+variable "private_subnet_1" {
+    default = "10.0.96.0/19"
 }
-variable "private-subnet-2" {
-    default =
+variable "private_subnet_2" {
+    default ="10.0.128.0/19"
 }
-variable "private-subnet-3" {
-    default =
+variable "private_subnet_3" {
+    default ="10.0.160.0/19"
 }
+
+
+# 10      .0       .0       .0  
+# 11111111.11111111.11111111.11111111
+# 255     .255     .255     .255
+
+# 128
+# 64
+# 32
+# 16
+# 8
+# 4
+# 2
+# 1
+
+# 32,64,96,128
